1039013cfdf443311990ea59d69b8b72b8a620a9
cist-b213-t-kato <cist.b213.t.kato@gmail.com>
2016-01-29 00:51:00 +0900

    stream使えばできた。しにたい

---
+++ src/NoOverlap.java
@@ -0,0 +1,23 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoOverlap {
+	public static void main(String[] args) {
+
+		NoOverlap main = new NoOverlap();
+
+		List<Integer> originList = new ArrayList<>(Arrays.asList(1, 2, 4, 2, 3, 2));
+		List<Integer> transformedList = originList.stream().distinct().collect(Collectors.toList());
+
+		for (int a : transformedList) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+
+	}
+
+}
