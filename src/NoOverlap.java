72e46eb9886887b6f78e1a539c18eaaf80359237
cist-b213-t-kato <cist.b213.t.kato@gmail.com>
2016-01-29 00:53:23 +0900

    stream使ってできた。しにたい

--- src/NoOverlap.java
+++ src/NoOverlap.java
@@ -11,6 +11,8 @@ public class NoOverlap {
 		NoOverlap main = new NoOverlap();
 
 		List<Integer> originList = new ArrayList<>(Arrays.asList(1, 2, 4, 2, 3, 2));
+
+		//リスト内の重複を排除する
 		List<Integer> transformedList = originList.stream().distinct().collect(Collectors.toList());
 
 		for (int a : transformedList) {
