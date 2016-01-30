d196c8801758225b699bf9a5cdccf01d47e855df
cist-b213-t-kato <cist.b213.t.kato@gmail.com>
2016-01-31 00:36:08 +0900

    辛辣

--- src/checkinstance/Workers.java
+++ src/checkinstance/Workers.java
@@ -1,31 +1,38 @@
 package checkinstance;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author b2130480
  */
-public class Main {
+public class Workers {
+    
+    public static void main(String[] args){
+        List<Human> humans = Arrays.asList(
+            new Firefighter(),
+            new Policeofficer(),
+            new Engineer(),
+            new Neet()
+        );
+        
+        for(Human h : humans){
+            Call(h);
+        }
+        
+    }
+    
     public static void Call(Human h){
-        if(h instanceof Neet){
-            System.out.println("働けニート");
-        }else if(h instanceof Firefighter){
+    	if(h instanceof Firefighter){
             System.out.println("お疲れさまです消防士さん");
         }else if(h instanceof Policeofficer){
             System.out.println("お疲れさまですおまわりさん");
         }else if(h instanceof Engineer){
             System.out.println("お疲れさまですエンジニアさん");
+        }else if(h instanceof Neet){
+            System.out.println("働けニート");
         }
-    }
-    
-    public static void main(String[] args){
-        Human hs[] = {
-            new Firefighter(), new Policeofficer(), new Engineer(), new Neet()
-        };
-        
-        for(Human h : hs){
-            Call(h);
-        }
-        
     }
     
 }
